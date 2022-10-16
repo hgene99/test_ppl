a23e57223ed3ea94e96a51d0b235c8a3601badda
hgene0929 <hgene0929@gmail.com>
2022-10-16 22:42:44 +0900

    :sparkles: 웹툰 이미지 크롤링 API 구현

--- app.py
+++ app.py
@@ -7,5 +7,15 @@ app = Flask(__name__, template_folder='templates')
 def index():
     return render_template('index.html')
 
+@app.route('/crawling_form')
+def crawling_form():
+    return render_template('crawl_cartoon_form.html')
+
+@app.route('/crawling', methods=['POST'])
+def crawling():
+    data = request.form
+    crawl_cartoon(data['url'], data['name'])
+    return jsonify({'code':'200'})
+
 if __name__ == '__main__':
     app.run(debug=True)
